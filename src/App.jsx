17fffbb24f60f487cf19cf96321541a01f6cd28f
ParthaChangmai <partha.partha.changmai@gmail.com>
2022-06-07 23:33:41 +0530

    removed demo table

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import './App.css';
-import DemoTable from './components/demoTable';
 
 import ViewTable from './components/ViewTable';
 
@@ -7,7 +6,6 @@ function App() {
 	return (
 		<div className="App">
 			<ViewTable />
-			{/* <DemoTable /> */}
 		</div>
 	);
 }
